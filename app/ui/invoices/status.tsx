32a79f36ada71697cfe8693066088b658f58b7e8
timetam <timetam24@naver.com>
2024-02-02 19:59:53 +0900

    Chapter 2: CSS Styling

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -6,10 +6,11 @@ export default function InvoiceStatus({ status }: { status: string }) {
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
+
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
-        },
+        }, // There may be cases where you may need to conditionally style an element based on state or some other condition. clsx is a library that lets you toggle class names easily.
       )}
     >
       {status === 'pending' ? (
